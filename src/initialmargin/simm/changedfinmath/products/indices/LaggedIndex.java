2554649fafd425f65b0e04b5a468004583702a0d
Mario <Mario@MarioV.localdomain>
2017-10-09 00:15:07 +0200

    Changes in finmath-lib classes to equip LMM with net.finmath.analytic.model.curves

---
+++ src/initialmargin/simm/changedfinmath/products/indices/LaggedIndex.java
@@ -0,0 +1,50 @@
+/*
+ * Created on 06.12.2009
+ *
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ */
+package initialmargin.simm.changedfinmath.products.indices;
+
+import java.util.Set;
+
+import initialmargin.simm.changedfinmath.LIBORModelMonteCarloSimulationInterface;
+import initialmargin.simm.changedfinmath.products.components.AbstractProductComponent;
+import net.finmath.exception.CalculationException;
+//import net.finmath.montecarlo.interestrate.LIBORModelMonteCarloSimulationInterface;
+//import net.finmath.montecarlo.interestrate.products.components.AbstractProductComponent;
+import net.finmath.stochastic.RandomVariableInterface;
+
+/**
+ * A time-lagged index paying index(t+fixingOffset)
+ * 
+ * @author Christian Fries
+ */
+public class LaggedIndex extends AbstractIndex {
+
+	private static final long serialVersionUID = 4899043672016395530L;
+
+	final AbstractProductComponent	index;
+	final double					fixingOffset;
+
+	/**
+	 * Creates a time-lagged index paying index(t+fixingOffset).
+	 * 
+	 * @param index An index.
+	 * @param fixingOffset Offset added to the fixing (evaluation time) of this index to fix the underlying index.
+	 */
+	public LaggedIndex(AbstractProductComponent index, double fixingOffset) {
+		super();
+		this.index			= index;
+		this.fixingOffset	= fixingOffset;
+	}
+
+	@Override
+	public Set<String> queryUnderlyings() {
+		return index.queryUnderlyings();
+	}
+
+	@Override
+	public RandomVariableInterface getValue(double evaluationTime, LIBORModelMonteCarloSimulationInterface model) throws CalculationException {
+		return index.getValue(evaluationTime + fixingOffset, model);
+	}
+}
