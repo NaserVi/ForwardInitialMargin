bd7905245f82bf9054620eb414ce64c4491b323e
Mario <Mario@10.180.50.31>
2017-11-11 21:43:05 +0100

    tenor refinement inclusion

---
+++ src/net/finmath/montecarlo/interestrate/modelplugins/TermStructureCovarianceModelInterface.java
@@ -0,0 +1,15 @@
+/*
+ * (c) Copyright Christian P. Fries, Germany. All rights reserved. Contact: email@christian-fries.de.
+ *
+ * Created on 24.12.2016
+ */
+package net.finmath.montecarlo.interestrate.modelplugins;
+
+/**
+ * A base class and interface description for the instantaneous covariance of
+ * an forward rate interest rate model.
+ * 
+ * @author Christian Fries
+ */
+public interface TermStructureCovarianceModelInterface extends TermStructureTenorTimeScalingInterface, TermStructureFactorLoadingsModelInterface {
+}
